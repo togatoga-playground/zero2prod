9827675ce617f7790a054f603c205375781c05d5
togatoga <togasakitogatoga@gmail.com>
2022-11-07 22:10:57 +0900

    Fix tests

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -7,9 +7,16 @@ use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app = spawn_app().await;    
+    let app = spawn_app().await;
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
     let response = app.post_subscriptions(body.into()).await;
     assert_eq!(200, response.status().as_u16());
 
