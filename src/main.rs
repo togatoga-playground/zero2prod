409dd9a49cc9cebfe1a40cdc6805d9c843a083b1
togatoga <hitoshitogasaki@gmail.com>
2023-05-10 22:59:49 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use zero2prod::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("Hello {}!", &name)
-}
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run()?.await
 }
